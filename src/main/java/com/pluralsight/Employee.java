47269c4af9b10e27f6d46df576fea8cf5e78c386
Mari-art-rgb <meggisonariela@gmail.com>
2026-04-22 11:42:24 -0400

    Completed.

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -1,4 +1,56 @@
 package com.pluralsight;
 
 public class Employee {
+    private String employeeId;
+    private String name;
+    private int hoursWorked;
+    private double payRate;
+
+
+
+    // This is a constructor
+    public Employee (String employeeId, String name, int hoursWorked, double payRate){
+    this.employeeId = employeeId;
+    this.name = name;
+    this.hoursWorked = hoursWorked;
+    this.payRate = payRate;
+
+ }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(int hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public double getGrossPay(){
+        return this.payRate * this.hoursWorked;
+    }
+
 }
--- src/main/java/com/pluralsight/payrollReader.java
+++ src/main/java/com/pluralsight/payrollReader.java
@@ -1,4 +1,45 @@
 package com.pluralsight;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
+
+import java.io.*;
+import java.util.regex.Pattern;
 
 public class payrollReader {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            FileReader fileReader = new FileReader("src/main/resources/employees.csv");
+            BufferedReader buffReader = new BufferedReader(fileReader);
+
+            String input;
+            buffReader.readLine();
+
+            while(input = buffReader.readLine()) != null) {
+                String[] splitData = input.split(Pattern.quote("|"));
+                int id = Integer.parseInt(splitData[0]);
+                String name = splitData[1];
+                double hoursWorked = Double.parseDouble(splitData[2]);
+                double payRate = Double.parseDouble(splitData[3]);
+
+                System.out.println(id);
+                System.out.println(name);
+            }
+
+            buffReader.close();
+
+
+        }catch(FileNotFoundException e) {
+            e.printStackTrace();
+        }catch (IOException e) {
+          e.printStackTrace();
+
+
+        }
+
+
+    }
+
 }
